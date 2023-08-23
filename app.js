6227f1c6596f0231c2889e36083f9a33a6561097
Harsh <142956867+rajaji89999@users.noreply.github.com>
2023-08-23 19:23:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,9 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(cors());
 app.use(helmet());
 
-if (process.env.NODE_ENV === 'development') {
-  app.use(morgan('dev'));
-}
+app.use(morgan('dev'));
 
 app.use(express.json({ limit: '10kb' }));
 app.use(cookieParser());
